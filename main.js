fbbcfbe1e13755d5dc89cce22dab2c3eaa87e729
Heather Thacker <hhthacker@gmail.com>
2017-02-11 14:01:05 -0600

    initial structure

---
+++ main.js
@@ -0,0 +1,27 @@
+
+// print spaces: 
+// treeHeight - (i +1)
+
+// print characters:
+// (2 * 1) + 1 
+
+var treeHeight;
+var treeCharacter;
+var i = {treeHeight: treeHeight.value, character: treeCharacter.value};
+
+// Once the user enters in a number, 
+// and a character, the user can either then just press the enter key 
+// (as long as the cursor is in one of the input fields), 
+// or click a button that is labeled "Grow your tree" 
+// and the tree should be shown in the console. 
+// This requires you to add an event listener to the button, 
+// as well as an event listener for the enter/return key.
+
+
+function tree(i) {
+	for (i[treeHeight] )
+}
+
+
+button.addEventListener("click", tree);
+console.log(tree);
