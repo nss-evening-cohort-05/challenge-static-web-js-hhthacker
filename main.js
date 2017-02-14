2b12cc442fed5aa12d3c57b43574dff8869ac2fb
Heather Thacker <hhthacker@gmail.com>
2017-02-13 22:41:46 -0600

    working click events

--- main.js
+++ main.js
@@ -3,34 +3,27 @@
 // treeHeight - (i +1)
 
 // print characters:
-// (2 * 1) + 1 
+// (2 * i) + 1 
 
 var button = document.getElementById("grow");
 var treeHeight = document.getElementById("treeHeight");
 var treeCharacter = document.getElementById("treeCharacter");
-var i = {treeHeight, treeCharacter};
+var myTree = {treeHeight, treeCharacter};
 
-// Once the user enters in a number, 
-// and a character, the user can either then just press the enter key 
-// (as long as the cursor is in one of the input fields), 
-// or click a button that is labeled "Grow your tree" 
-// and the tree should be shown in the console. 
-// This requires you to add an event listener to the button, 
-// as well as an event listener for the enter/return key.
+function printSpaces(myTree) {
+	var height = treeHeight.value;
+	return treeHeight - (treeHeight + 1);
+}
 
+function printCharacters(myTree) {
+	var character = treeCharacter.value;
+	return (2 * treeHeight) + 1;
+}
 
-function growTree(i) {
-	for (var i = 0; i < treeCharacter; i ++) {
-		return (treeHeight - (i +1)) + (treeCharacter (2 * 1) + 1);
+function growTree(clickEvent) {
+		return printSpaces(); printCharacters();
 	}
-	console.log(growTree); 
-}
 
-
-
-function button() {
-console.log(treeHeight, treeCharacter);
-}
 
 function enterKeyPress(keypress) {
 	if (keypress.which === 13) {
@@ -38,6 +31,6 @@ function enterKeyPress(keypress) {
 	}
 }
 
+console.log(growTree);
 button.addEventListener("click", growTree);
-
-console.log(tree);
+document.addEventListener("keypress", enterKeyPress);
